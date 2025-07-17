c0df76d007ca2833895c2365b5514aa90e5c0eb2
Ralph Largo <devlargs@gmail.com>
2025-07-18 07:46:44 +0800

    feat: added constants for eggs

---
+++ src/constants/eggs.ts
@@ -0,0 +1,31 @@
+import type { Variants } from "../types/variants";
+
+export const EGGS: Array<{
+  name: string;
+  variant: Variants;
+}> = [
+  {
+    name: "Common Egg",
+    variant: "Common",
+  },
+  {
+    name: "Common Summer Egg",
+    variant: "Common",
+  },
+  {
+    name: "Rare Summer Egg",
+    variant: "Rare",
+  },
+  {
+    name: "Mythical Egg",
+    variant: "Mythical",
+  },
+  {
+    name: "Paradise Egg",
+    variant: "Mythical",
+  },
+  {
+    name: "Bug Egg",
+    variant: "Divine",
+  },
+];
